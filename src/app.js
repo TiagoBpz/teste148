fb93348ba2fd7b15ab00578115b921080e9748c9
TiagoBpz <tiagobrasileiroplt@gmail.com>
2025-05-19 16:32:58 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import routes from "./routes.js";
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(routes)
+
+export default app;
